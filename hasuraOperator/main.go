dc43b89458514bd8b1066a84c4119506037890d9
Stephen Barbour <sbarbour@barbourandco.com>
2022-12-20 15:49:19 -0500

    push operator and dockerfile

--- hasuraOperator/main.go
+++ hasuraOperator/main.go
@@ -1,26 +1,49 @@
 package main
 
 import (
+    "context"
     "fmt"
     "os"
     "time"
-
+    corev1 "k8s.io/api/core/v1"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/rest"
+    "k8s.io/client-go/tools/cache"
 )
 
 func main() {
     config, err := rest.InClusterConfig()
     if err != nil {
-        fmt.Fprintf(e)
+        panic(err.Error())
     }
 
     clientset, err := kubernetes.NewForConfig(config)
     if err != nil {
-        fmt.Fprintf(e)
+        panic(err.Error())
     }
 
+    // create a watcher for the ConfigMap
     configMapInformer := cache.NewSharedIndexInformer(
         &cache.ListWatch{
-            ListFunc: func(options metav1),
+            ListFunc: func(options metav1.ListOptions) (object runtime.Object, e error) {
+                return clientset.CoreV1().ConfigMaps("default").List(context.TODO(), options)
+            },
+            WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+                return clientset.CoreV1().ConfigMaps("default").Watch(context.TODO(), options)
+            },
+            },
+            &corev1.ConfigMap{},
+            0,
+            cache.Indexers{},
+            )
+
+    // create a controller for the deployment
+    deploymentController := cache.NewSharedIndexInformer(
+        &cache.ListWatch{
+            ListFunc: func(options metav1.ListOptions) (object runtime.Object, e error) {
+                return clientset.AppsV1().Deployments("default").List(context.TODO(), options)
+            }
+
+            WatchFunc: func(options metav1.ListOptions) (watch.Interface, error)
         }
-        )
-}
